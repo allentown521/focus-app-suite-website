529c439dfffb2425b3944f4f96599304b85a5924
allentown <tangxiangpi@gmail.com>
2025-02-26 22:34:36 +0800

    ci: 更新网站 URL 为 focusapps.app
    - 修改 next-sitemap 配置文件中的 siteUrl
    - 更新 robots.txt 文件中的 Host 和 Sitemap URL
    - 修改 sitemap-0.xml 和 sitemap.xml 中的 URL

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.SITE_URL || 'https://focus.hk.cn',
+    siteUrl: process.env.SITE_URL || 'https://focusapps.app',
     generateRobotsTxt: true, // (optional)
     // ...other options
   }
